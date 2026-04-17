197d42eb28e702993392d7913a3faf4c3759a390
harsh07kashyap <kashyap7.harsh@gmail.com>
2026-04-17 18:09:57 +0530

    manual reply stage 9

--- config/mail.js
+++ config/mail.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
